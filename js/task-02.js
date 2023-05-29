733f063e544238656314b759b1f742d12ec9caa2
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-05-29 19:54:20 +0200

    Updata_task_2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const listItems = ingredients.map(el => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = el;
+  return listItem;
+});
+
+listItems.forEach(el => {
+  list.appendChild(el);
+});
+console.log(list);
