bff2f844d0b87a9480e7960cb2c2c1d0b972e015
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-06-02 21:56:42 +0200

    task_9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const hexColorBtn = document.querySelector('.change-color');
+const hexColor = document.querySelector('.color');
+
+hexColorBtn.addEventListener('click', onClick);
+// hexColorBtn.removeEventListener('click', onClick);
+function onClick(evt) {
+  hexColor.textContent.innerHTML += getRandomHexColor;
+  console.log('hello');
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor());
